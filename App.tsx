6b7c8f5d12b99ab33639c5be1c61ab97a63ea035
erasmo-costa <ecosta@viptech.com.br>
2023-02-24 17:34:08 -0300

    Feature: Splash screen and app preparated to release

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-//import SplashScreen from 'react-native-splash-screen';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import {TasksProvider} from './src/context/useTasks';
 import {Home} from './src/home';
 
 const App = () => {
-  // React.useEffect(() => {
-  //   SplashScreen.hide();
-  // }, []);
+  useEffect(() => {
+    //SplashScreen.hide();
+  }, []);
 
   return (
     <TasksProvider>
